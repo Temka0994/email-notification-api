c3f31c063e808a84737dc79a265008fc67b10f07
Temka0994 <pleskovartem878@gmail.com>
2025-04-25 18:14:55 +0300

    commit

--- src/api/notice.py
+++ src/api/notice.py
@@ -1,3 +1,4 @@
+import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -10,16 +11,22 @@ router = APIRouter(tags=["Notice"])
 
 @router.post('/notice/', summary="Schedules to send a notice.")
 async def send_notice(data: NoticeSchema):
+    template_path = os.path.join("src", "templates", "notice.html")
+    with open(template_path, "r", encoding="utf-8") as file:
+        html_template = file.read()
 
     msg = MIMEMultipart()
     msg['Subject'] = data.subject
     msg['From'] = from_mail
     msg['To'] = data.to_mail
-    msg.attach(MIMEText(data.message))
+
+    html_content = html_template.replace("{{ message }}", data.message)
+
+    msg.attach(MIMEText(html_content, "html"))
 
     with smtplib.SMTP(host, port) as server:
         server.starttls()
         server.login(from_mail, from_mail_password)
         server.send_message(msg)
 
-
+    return {"message": "ok"}
