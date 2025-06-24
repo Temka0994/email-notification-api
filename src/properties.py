ea5375ef6341e8c10a3d21c7665b737628dfb5f5
Temka0994 <pleskovartem878@gmail.com>
2025-06-24 13:21:10 +0300

    commit

---
+++ src/properties.py
@@ -0,0 +1,9 @@
+from_mail = "Insert you data here."
+from_mail_password = "Insert you data here."
+
+host = "Insert you data here."
+port = "Insert you data here."
+
+SECRET_KEY = 'Insert you data here.'
+
+DATABASE_URL = "Insert you data here."
